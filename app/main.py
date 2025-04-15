f23179967705defe1068229cf8f964b0d84c4f84
alangajardomedina <alan.gajardo@profesor.duoc.cl>
2025-04-15 11:39:39 -0400

    modificacion - se agrega conexion con oracle

--- app/main.py
+++ app/main.py
@@ -1,20 +1,51 @@
 #Librería que nos permite crear una api:
 #FastAPI es la función para crear la API:
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+#librería para realizar conexión con oracle:
+import cx_Oracle
+from pydantic import BaseModel
 
 #acabamos de crear una variable app, del tipo FastAPI
 api = FastAPI()
 
-#Vamos a crear algunos métodos de la API:
-#get, post, put, patch, delete
-@api.get("/")
-def root():
-    return {"mensaje": "Hola mundo desde API 2"}
+#Conexión con oracle:
+def get_conexion():
+    try:
+        dsn = cx_Oracle.makedsn(
+            "localhost",
+            1521,
+            service_name="orcl.duoc.com.cl"
+        )
+        conexion = cx_Oracle.connect(
+            user="ejemplo_fastapi",
+            password="ejemplo_fastapi",
+            dsn=dsn
+        )
+        return conexion
+    except Exception as e:
+        print("Error al conectar con Oracle:",e)
+        raise
 
-@api.get("/{id}")
-def getAuto(id: int):
-    autos = {
-        1: {"marca": "Toyota", "modelo": "Supra"},
-        2: {"marca": "Nissan", "modelo": "V16"}
-    }
-    return autos.get(id)
+#Vamos a crear un modelo para Usuarios:
+class Usuario(BaseModel):
+    rut: int
+    pnombre: str
+    apaterno: str
+    amaterno: str
+    email: str
+
+@api.get("/usuarios")
+def get_usuarios():
+    try:
+        cone = get_conexion()
+        cursor = cone.cursor()
+        cursor.execute("SELECT rut, pnombre, apaterno, amaterno, email FROM usuario")
+        rows = cursor.fetchall()
+        usuarios = [Usuario(rut=u[0], pnombre=u[1], apaterno=u[2], amaterno=u[3], email=u[4]) for u in rows]        
+        return usuarios
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"Error al obtener usuario: {e}"
+        )
+    
